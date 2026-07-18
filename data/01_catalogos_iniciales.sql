7736f628c65ccd32759ceaa19450070a7cdb4d46
alovillagar24 <villagar11@gmail.com>
2026-07-18 14:38:09 -0600

    Create 01_catalogos_iniciales.sql

---
+++ data/01_catalogos_iniciales.sql
@@ -0,0 +1,7 @@
+--productos
+INSERT INTO productos (nombre, precio_unitario, categoria) VALUES 
+('Cafe', 25, 'Bebidas'), ('Te', 20, 'Bebidas'), ('Torta', 40, 'Alimentos'), ('Pan', 10, 'Repostería'),
+('Jugo', 30, 'Bebidas'), ('Dona', 15, 'Repostería'), ('Sopa', 35, 'Alimentos'), ('Taco', 25, 'Alimentos'),
+('Soda', 20, 'Bebidas'), ('Flan', 20, 'Repostería'), ('Copa', 45, 'Repostería'), ('Agua', 15, 'Bebidas'),
+('Pizza', 60, 'Alimentos'), ('Wrap', 45, 'Alimentos'), ('Muffin', 25, 'Repostería'), ('Chai', 35, 'Bebidas'),
+('Helado', 30, 'Repostería'), ('Bagel', 35, 'Alimentos'), ('Batido', 50, 'Bebidas'), ('Galleta', 10, 'Repostería');
