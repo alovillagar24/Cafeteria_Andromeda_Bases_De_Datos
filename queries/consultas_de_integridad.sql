25d9ef21eb00f8821122a3d8d974334bec29371c
alovillagar24 <villagar11@gmail.com>
2026-07-18 19:07:38 -0600

    Update consultas_de_integridad.sql

--- queries/consultas_de_integridad.sql
+++ queries/consultas_de_integridad.sql
@@ -8,6 +8,13 @@ JOIN productos pr ON dp.id_producto = pr.id_producto;
 -- Validación de tipos de datos (no devuelve registros)
 SELECT * FROM productos WHERE precio_unitario <= 0;
 
+-- Prueba de restricciones: retorna error (Foreign Key Constraint Fails)
+INSERT INTO detalles_pedidos (id_pedido, id_producto, cantidad, precio_cobrado) 
+VALUES (1, 999, 1, 10.00);
+-- El resultado esperado es que el sistema devuelva un error “Foreign Key Constraint Fails”, validando que la base de datos protege la integridad impidiendo datos inválidos.
+
+-- Validación de campos obligatorios: retorna error por la restricción NOT NULL aplicada desde el CREATE table
+INSERT INTO clientes (nombre_completo) VALUES (NULL);
 
 -- Trigger de auditoria trg_auditoria_precio
 SELECT * FROM productos WHERE id_producto = 1; -- 01. Identifica un producto existente.
